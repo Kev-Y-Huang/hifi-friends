8697d546363a85ea459a0f4f3f81e17283a05ccb
Ashley Zhuang <ashleyzhuang01@gmail.com>
2023-04-27 23:11:05 -0400

    Refactor client upload code

--- src/client-upload.py
+++ src/client-upload.py
@@ -1,34 +1,42 @@
 import socket
 import os
 
-s = socket.socket()
-host = socket.gethostname()
-port = 1538
-s.connect((host, port))
-path = "samples"
-directory = os.listdir(path)
+HOST = socket.gethostname()
+PORT = 1538
 
-while True:
-    file_path = input()
+class Client:
+    def __init__(self, host=HOST, port=PORT):
+        self.s = socket.socket()
+        self.host = host
+        self.port = port
 
-    filename = os.path.basename(file_path)
-    size = len(filename)
-    size = bin(size)[2:].zfill(16) # encode filename size as 16 bit binary, limit your filename length to 255 bytes
+    def upload_file(self, file_path):
+        filename = os.path.basename(file_path)
+        size = len(filename)
+        size = bin(size)[2:].zfill(16) # encode filename size as 16 bit binary, limit your filename length to 255 bytes
 
-    s.send(size.encode())
-    s.send(filename.encode())
+        self.s.send(size.encode())
+        self.s.send(filename.encode())
 
-    filesize = os.path.getsize(file_path)
-    filesize = bin(filesize)[2:].zfill(32) # encode filesize as 32 bit binary
-    s.send(filesize.encode())
+        filesize = os.path.getsize(file_path)
+        filesize = bin(filesize)[2:].zfill(32) # encode filesize as 32 bit binary
+        self.s.send(filesize.encode())
 
-    file_to_send = open(file_path, 'rb')
+        file_to_send = open(file_path, 'rb')
 
-    l = file_to_send.read()
-    s.sendall(l)
-    file_to_send.close()
-    print('File Sent')
+        l = file_to_send.read()
+        self.s.sendall(l)
+        file_to_send.close()
+        print('File Sent')
 
-    
 
-s.close()
+    def connect_to_server(self):
+        self.s.connect((self.host, self.port))
+
+        while True:
+            file_path = input()
+            self.upload_file(file_path)
+
+if __name__ == "__main__":
+    client = Client()
+    client.connect_to_server()
